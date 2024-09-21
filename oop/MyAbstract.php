80ff06e884ff1a3a2dfc1b25c36f4c5e2e0edaec
Thanhdat328 <92036923+Thanhdat328@users.noreply.github.com>
2024-09-21 10:23:08 +0700

    oop 21/9

---
+++ oop/MyAbstract.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class AbsA {
+    // Khai báo  định nghĩa hàm
+    public function Declare(){
+        echo 'abcd';
+    }
+    // Khai báo ko định nghĩa hàm
+    abstract function NotDeclare();
+    // Final trong abstract
+    
+     
+   
+}
+
+abstract class abstract1{
+    abstract public function func_from_Ab1_no_body();
+}
+abstract class AbsB {
+    abstract function runB();
+    
+}
+
+abstract class AbsC {
+    abstract function runC();
+    
+}
+
+
---
+++ oop/MyInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+interface IA {
+  public function NotDeclare();
+
+}
+
+interface IB{
+   
+
+}
+interface IC{
+   
+
+}
+
+interface  Interface1{
+  public function func_from_Int1();
+
+}
+
+ interface Interface2 {
+  public function func_from_Int2();
+
+ }
+
+ interface Interface3 {
+  public function func_from_Int3();
+
+ }
